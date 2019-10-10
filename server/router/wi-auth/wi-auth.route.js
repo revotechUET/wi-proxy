1df143991163fcbb61f4fe720b904dbc00de08ad
Nam Phan <sockyone>
2019-10-10 15:55:33 +0700

    push

--- server/router/wi-auth/wi-auth.route.js
+++ server/router/wi-auth/wi-auth.route.js
@@ -12,6 +12,8 @@ let proxyConfig = require('./wi-auth.config');
 
 let wiAuthCloudUrl = process.env.WI_AUTH_CLOUD || config.get("cloud.wi_auth") || 'https://users.i2g.cloud';
 let wiAuthLocalUrl = process.env.WI_AUTH_LOCAL || config.get("local.wi_auth") || 'http://localhost:2999';
+let wiSyncLocalUrl = process.env.WI_SYNC_LOCAL || config.get("wi_sync") || 'http://localhost:9099';
+let wiBackendLocalUrl = process.env.WI_BACKEND_LOCAL || config.get("wi_backend") || 'http://localhost:3000';
 
 let proxyRouteOf = require('./../../helper/proxy.helper');
 let proxyRoute = proxyRouteOf(proxyConfig);
@@ -28,6 +30,9 @@ router.post('/login', async (req, res)=>{
     //try if there is account exist or not
     try {
         let response = await axios.post(wiAuthLocalUrl + '/login', req.body);
+        
+        let username = req.body.username;
+        let password = req.body.password;
 
         //if this account is not exist in local
         if (response.data.reason.toString() === 'User is not exists.') {
@@ -42,8 +47,8 @@ router.post('/login', async (req, res)=>{
                 let idCompany = responseAuthCloud.data.content.company.idCompany || null;
                 await makeCompanyExist(responseAuthCloud.data.content.company, token);
                 let responseAuthLocal = await axios.post(wiAuthLocalUrl + '/user/new', {
-                    username: req.body.username,
-                    password: req.body.password,
+                    username: username,
+                    password: password,
                     idCompany: idCompany,
                     status: 'Active'
                 }, {
@@ -52,13 +57,39 @@ router.post('/login', async (req, res)=>{
                         Authorization: token
                     }
                 });
+                axios.post(wiBackendLocalUrl + '/update', {}, {
+                    headers:{
+                        Authorization: token
+                    }
+                }).then((rs)=>{
+                    if (parseInt(rs.data.code) === 200) { 
+                        axios.post(wiSyncLocalUrl + '/sync/start', {
+                            username: username
+                        }).then((rs=>{
+                            console.log('Sync', username + ':', rs.data);
+                        })).catch(e=>{
+                            console.log(e);
+                        });
+                    }
+                }).catch(e=>{
+                    console.log(e);
+                });
                 if (responseAuthLocal.data.code === 200) {
                     let anotherResponse = await axios.post(wiAuthLocalUrl + '/login', req.body);
                     res.status(anotherResponse.data.code).json(anotherResponse.data);
                     return;
                 }
             } else {
-                res.status(responseAuthCloud.data.code).json(responseAuthCloud.data);
+                let resFromSync = await axios.post(WI_SYNC_LOCAL + '/sync/status', {username: username});
+                if (resFromSync.data.payload.status.toString() = 'SYNCING') {
+                    res.status(512).json({
+                        code: 512,
+                        reason: 'Database is syncing, can not login, please wait for ~10->30mins',
+                        payload: {}
+                    });
+                } else {
+                    res.status(responseAuthCloud.data.code).json(responseAuthCloud.data);
+                }
                 return;
             }
         }
